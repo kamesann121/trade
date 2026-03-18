4722e5e1741708a1a77f397ef287ef4c3442bb38
kamesann121 <futomitianzhong@gmail.com>
2026-03-18 11:48:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth',          require('./routes/auth'));
 app.use('/api/users',         require('./routes/users'));
 app.use('/api/items',         require('./routes/items'));
+app.use('/api/comments',      require('./routes/comments'));
 app.use('/api/exchange',      require('./routes/exchange'));
 app.use('/api/messages',      require('./routes/messages'));
 app.use('/api/reviews',       require('./routes/reviews'));
