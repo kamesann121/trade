300dbae93f192d4aa124878993b0e4f7cf69a9fb
kamesann121 <futomitianzhong@gmail.com>
2026-03-19 10:07:12 +0900

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -5,7 +5,9 @@ const messageSchema = new mongoose.Schema({
   sender:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   text:     { type: String, required: true, trim: true },
-  read:     { type: Boolean, default: false }
+  read:     { type: Boolean, default: false },
+  isAdmin:  { type: Boolean, default: false },  // 管理者からのメッセージ
+  senderName: { type: String, default: '' }      // 管理者表示名
 }, { timestamps: true });
 
 // conversationId は常に「小さいID_大きいID」で統一
